8546321fc702ba0be92a238824db8e230f06de01
matheusmanoelberto <matheusmanoel51@gmail.com>
2023-06-26 22:26:44 -0300

    Initial commit

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,23 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duracao: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adiquira o plano Plus+");
+        }
+    }
+}
+
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Infinitamente";
+musica1.artista = "Novo som";
+musica1.duracao = 300;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Isaias 6";
+musica2.artista = "Rodolf Abrante";
+musica2.duracao = 400;
+musica2.disponivel= false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
